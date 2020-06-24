b0e4deb95056c17037b981f5b5ce9c60274df1ef
psi <ryo.hirafuji@link-u.co.jp>
2020-06-24 21:19:48 +0900

    change install dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,9 @@ set -eux
 
 root_dir=$(readlink -f $(cd $(dirname $(readlink -f $0)) && cd .. && pwd))
 
-pkg-config --list-all
-
 echo "building ffmpeg"
 cd ${root_dir}/ffmpeg
 fakeroot debian/rules clean
 fakeroot debian/rules configure
-mkdir -p debian/tmp
-chroot debian/tmp fakeroot debian/rules build
+fakeroot debian/rules build
 fakeroot debian/rules binary
