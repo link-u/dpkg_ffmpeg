f1342f1a743999fd0b7ff8f4cc8d1870cb4f1b0b
psi <ryo.hirafuji@link-u.co.jp>
2020-06-25 05:48:21 +0900

    need './'

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ scripts_dir=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
 root_dir=$(cd ${scripts_dir} && cd .. && pwd)
 cd ${root_dir}
 
-apt install -y artifact/*.deb
+apt install -y ./artifact/*.deb
 apt show ffmpeg-tools
 which ffmpeg
 which ffprobe
