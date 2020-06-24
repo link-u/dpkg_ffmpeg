f25cd6e16ec49dd5cbdb6515a5808a775b426952
psi <ryo.hirafuji@link-u.co.jp>
2020-06-24 13:46:29 +0900

    ffmpegのディレクトリで実行

--- scripts/create_changelog.sh
+++ scripts/create_changelog.sh
@@ -3,8 +3,12 @@
 set -eu
 
 ## git リポジトリ上の root のパスを取得
-root_dir=$(readlink -f $(cd $(dirname $(readlink -f $0)) && cd .. && pwd))
 ffmpeg_dir=$(readlink -f $(cd $(dirname $(readlink -f $0)) && cd .. && cd ffmpeg && pwd))
+
+cd ${ffmpeg_dir}
+
+## 最新のコミットをタグ含め取得
+git fetch origin --all
 
 ## HEAD のコミットID と HEAD の時のタグを取得
 git_describe="$(git describe --tags)"
@@ -13,5 +17,5 @@ git_describe="$(git describe --tags)"
 code_name="$(lsb_release -cs)"
 
 ## changelog の修正
-sed -i -r "s/%DATE%/$(LC_ALL=C TZ=JST-9 date '+%a, %d %b %Y %H:%M:%S %z')/g" "${ffmpeg_dir}/debian/changelog"
-sed -i -r "s/%VERSION%/${git_describe:1}.$(TZ=JST-9 date +%Y%m%d)+${code_name}/g" "${ffmpeg_dir}/debian/changelog"
+sed -i -r "s/%DATE%/$(LC_ALL=C TZ=JST-9 date '+%a, %d %b %Y %H:%M:%S %z')/g" "debian/changelog"
+sed -i -r "s/%VERSION%/${git_describe:1}.$(TZ=JST-9 date +%Y%m%d)+${code_name}/g" "debian/changelog"
